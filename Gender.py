d25fbd702d1fdfd0249ea5b5d110b22e96738a01
omare334 <omareweis123@gmail.com>
2022-12-01 11:11:49 +0000

    Create Gender.py
    individual  brilliance (actually this time) by moi

---
+++ Gender.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python
+# coding: utf-8
+# In[39]:
+
+
+import faker 
+import numpy as np
+import pandas as pd
+import random 
+
+
+# In[29]:
+
+
+from faker import Faker
+
+
+# In[30]:
+
+
+fake = Faker()
+
+
+# In[42]:
+
+
+def make_workers(num):
+    random.seed(1)
+    gender_list = ['M', 'F']
+    fake_workers = [{'sample_id':np.random.randint(1,500),
+    'gender': np.random.choice(gender_list)}]   
+    return fake_workers
+
+
+# In[43]:
+
+
+worker_df = pd.DataFrame(make_workers(num=1))
+
+i=1
+while i < 500:
+    worker_df = worker_df.append(pd.DataFrame(make_workers(num=1)))
+    i += 1
+
+worker_df.head(100)
+
+
+# In[45]:
+print(worker_df['gender'].tolist().count('F'))
+print(worker_df)
+
